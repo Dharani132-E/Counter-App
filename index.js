a2a827861477ebead2685214da31d54b22feb6d3
Dharani132-E <dharanie790@gmail.com>
2026-08-13 21:56:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let count=0;
+let incrementValue=document.getElementById("incrementBtn")
+let decrementValue=document.getElementById("decrementBtn")
+let resetValue=document.getElementById("resetBtn")
+let displayCount=document.getElementById("display")
+
+incrementValue.addEventListener("click",increment);
+function increment(){
+count++;
+display.innerHTML=count;
+}
+
+decrementValue.addEventListener("click",decrement);
+function decrement(){
+count--;
+display.innerHTML=count;
+}
+
+resetValue.addEventListener("click",reset);
+function reset(){
+count=0;
+display.innerHTML=count;
+}
