23055e1beb74bd88a8236f970306a5c1a6fe0d1f
Dharani132-E <dharanie790@gmail.com>
2026-08-20 18:21:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let count=0;
 let incrementValue=document.getElementById("incrementBtn")
 let decrementValue=document.getElementById("decrementBtn")
 let resetValue=document.getElementById("resetBtn")
-let displayValue=document.getElementById("display")
+let displaycount=document.getElementById("display")
 
 incrementValue.addEventListener("click",increment);
 function increment(){
